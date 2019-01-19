7ce628dee0b7171aad2e32cfbe8a5213dd56f40f
nohwnd <me@jakubjares.com>
2019-01-19 11:46:26 +0100

    Demo code for v5-alpha2

--- demo/scoping/InvocationOrder.Tests.ps1
+++ demo/scoping/InvocationOrder.Tests.ps1
@@ -5,28 +5,12 @@
         Write-Host Running BeforeAll
     }
 
-    Describe "a" {
-        It "b" { $true }
-    }
-
-    BeforeEach {
-        Write-Host Running BeforeEach
-    }
-    
     It "i" { 
         Write-Host Running It
     }
 
-    It "i" { 
-        Write-Host Running It2
-    }
-
-    AfterEach {
-        Write-Host Running AfterEach
-    }
-
     AfterAll { 
         Write-Host Running AfterAll
-        throw
     }
+    Write-Host Leaving Describe
 }
