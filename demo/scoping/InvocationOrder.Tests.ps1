1d7252f0801a64277b3ec989fc97472878b7fd74
nohwnd <me@jakubjares.com>
2019-01-19 07:04:51 +0100

    Quick scoping example

---
+++ demo/scoping/InvocationOrder.Tests.ps1
@@ -0,0 +1,32 @@
+﻿Describe "d" { 
+    Write-Host Running Describe
+
+    BeforeAll {
+        Write-Host Running BeforeAll
+    }
+
+    Describe "a" {
+        It "b" { $true }
+    }
+
+    BeforeEach {
+        Write-Host Running BeforeEach
+    }
+    
+    It "i" { 
+        Write-Host Running It
+    }
+
+    It "i" { 
+        Write-Host Running It2
+    }
+
+    AfterEach {
+        Write-Host Running AfterEach
+    }
+
+    AfterAll { 
+        Write-Host Running AfterAll
+        throw
+    }
+}
